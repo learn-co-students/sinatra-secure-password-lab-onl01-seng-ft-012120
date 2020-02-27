4444604a41ef222077ad64cbb5344d7be3608029
reddevilcero <reddevil_cero@hotmail.com>
2020-02-27 11:23:04 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,17 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
+    if params[:username].empty? || params[:password].empty?
+      redirect "/failure"
+    else
+      user = User.new(params)
+      if user.save
+        redirect "/login"
+      else
+        redirect "failure"
+      end
+    end
+
 
   end
 
@@ -33,6 +44,17 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    if params[:username].empty? || params[:password].empty?
+      redirect "/failure"
+    else
+      user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect '/account'
+      else
+        redirect '/failure'
+      end
+    end
   end
 
   get "/failure" do
